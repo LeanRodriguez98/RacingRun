bdf56f0ef6b30a8637c7a57b845f1e3efe53f5df
Unknown <lean1998@hotmail.com>
2018-09-10 23:05:43 -0300

    BezierTurn

---
+++ DesarrolloMixto/Assets/Scripts/BezierTurn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierTurn : MonoBehaviour {
+
+    private float t = 0;
+    private Vector3 p;
+    public Player player;
+    public GameObject p0;
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public int CurveSmoothness;
+
+    private void Awake()
+    {
+        player = Player.instance;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CarCenter")
+        {
+            Player.instance.
+                bezierTurn
+                = this;
+            TurnOn();
+        }
+    }
+
+    public Vector3 CalculateCubicBezierPoint()
+    {
+        if (t >= 1)
+            TurnOff();
+       p = ((1 - t) * (1 - t) * (1 - t)) * p0.transform.position + 3 * ((1 - t) * (1 - t)) * t * p1.transform.position + 3 * (1 - t) * (t * t) * p2.transform.position + (t * t * t) * p3.transform.position;
+       t += Time.deltaTime / CurveSmoothness;
+        return p;
+    }
+
+    public Vector3 LookAtPoint()
+    {
+        p = ((1 - t) * (1 - t) * (1 - t)) * p0.transform.position + 3 * ((1 - t) * (1 - t)) * t * p1.transform.position + 3 * (1 - t) * (t * t) * p2.transform.position + (t * t * t) * p3.transform.position;
+        return p;
+    }
+    private void TurnOn()
+    {
+        Player.instance.State = Player.States.Turn;
+    }
+
+    private void TurnOff()
+    {
+        Player.instance.State = Player.States.Forward;
+    }
+}
--- DesarrolloMixto/Assets/Scripts/Player.cs
+++ DesarrolloMixto/Assets/Scripts/Player.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour {
 
+    private Rigidbody rb;
+    private float Speed;
+    public enum States { Forward, Turn };
+    public States State;
+    public BezierTurn bezierTurn;
+    public Weapon WeaponLeft;
+    public Weapon WeaponRight;
+    public float MaxSpeed;
     #region Singleton
     public static Player instance;
     private void Awake()
@@ -18,25 +27,45 @@ public class Player : MonoBehaviour {
     }
     #endregion
 
-    public Weapon WeaponLeft;
-    public Weapon WeaponRight;
-    public float MaxSpeed;
-    private Rigidbody rb;
-    private float Speed;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        State = States.Forward;
     }
 
     private void Update()
     {
-        if (Speed < MaxSpeed)
-        {
-            Speed += Time.deltaTime/5;
-        }
-        rb.AddForce(-transform.right * Speed);
-      
+        Movement();
+        Fire();
+    }
 
+    private void Movement()
+    {
+        switch (State)
+        {
+            case States.Forward:
+                if (Speed < MaxSpeed)
+                {
+
+                    Speed += Time.deltaTime / 5;
+                }
+                transform.position += transform.forward * Speed * Time.deltaTime;
+                break;
+            case States.Turn:
+                Vector3 pos;
+                pos.x = bezierTurn.CalculateCubicBezierPoint().x;
+                pos.y = transform.position.y;
+                pos.z = bezierTurn.CalculateCubicBezierPoint().z;
+                transform.position = pos;
+                transform.LookAt(new Vector3(bezierTurn.LookAtPoint().x , transform.position.y , bezierTurn.LookAtPoint().z));
+                
+                break;
+           
+        }        
+    }
+
+    private void Fire()
+    {
         if (InputManager.instance.Fire())
         {
             WeaponLeft.Fire();
