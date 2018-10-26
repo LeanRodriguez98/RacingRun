bfaa7c9a18c0abe56b40b8a1803c6131ed740a09
Unknown <lean1998@hotmail.com>
2018-10-26 19:41:18 -0300

    ImoportedAssets + Prefabs

--- DesarrolloMixto/Assets/Scripts/Utilitis/Damageable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Damageable : MonoBehaviour {
-
-   public abstract void SetDamage(int damage);
-}
