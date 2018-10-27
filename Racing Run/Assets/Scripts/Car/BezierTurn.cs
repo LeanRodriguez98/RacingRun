5b8ab5cbc84944276f26d73e604a2d0d3818c842
Unknown <lean1998@hotmail.com>
2018-10-26 23:30:52 -0300

    CarClass + BezierTurn

---
+++ Racing Run/Assets/Scripts/Car/BezierTurn.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierTurn : MonoBehaviour {
+
+    private float t = 0;
+    private Vector3 p;
+    private float carRotation;
+    public Car carInstance;
+    public GameObject p0;
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public int CurveSmoothness;
+    public enum Direction {Left, Right};
+    public Direction direction;
+    private void Awake()
+    {
+        carInstance = Car.instance;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+   
+         if (other.tag == "CarCenter")
+         {
+            carInstance.bezierTurn = this;
+            p0.transform.position = Car.instance.transform.position;
+            if(direction == Direction.Left)
+            carRotation = carInstance.transform.rotation.eulerAngles.y - 90;
+            if(direction == Direction.Right)
+            carRotation = carInstance.transform.rotation.eulerAngles.y + 90;
+            TurnOn();
+         }
+        
+    }
+
+    public Vector3 CalculateCubicBezierPoint( float speed)
+    {
+        if (t >= 1)
+            TurnOff();
+        p = CalculatePoint();
+        t += Time.deltaTime * speed / CurveSmoothness;       
+        return p;
+    }
+
+    public Vector3 LookAtPoint()
+    {
+        p = CalculatePoint();
+        return p;
+    }
+
+    private Vector3 CalculatePoint()
+    {
+        p = ((1 - t) * (1 - t) * (1 - t)) * p0.transform.position + 3 * ((1 - t) * (1 - t)) *
+            t * p1.transform.position + 3 * (1 - t) * (t * t) * p2.transform.position +
+            (t * t * t) * p3.transform.position;
+        return p;
+    }
+
+
+    private void TurnOn()
+    {
+        if (carInstance.states == Car.States.Forward)
+            carInstance.states = Car.States.Turn;
+    }
+
+    private void TurnOff()
+    {
+        carInstance.states = Car.States.Forward;
+    }
+
+    public Vector3 GetFixedRotation()
+    {
+        Vector3 r = carInstance.transform.rotation.eulerAngles;
+        r.y = carRotation;
+        return r;
+    }
+   
+}
---
+++ Racing Run/Assets/Scripts/Car/Car.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public int life;
+    public float speed;
+    public float maxSpeed;
+    public float ascelerationMultipler;
+    public enum States {Forward,Turn};
+    public States states = States.Forward;
+    public static Car instance;
+    public BezierTurn bezierTurn;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start () {
+		
+	}
+
+	
+	void Update () {
+        if (speed < maxSpeed)
+            speed += Time.deltaTime * ascelerationMultipler;
+        switch (states)
+        {
+            case States.Forward:                
+                transform.position += transform.forward * speed * Time.deltaTime;
+                transform.Translate(Input.acceleration.x * speed * Time.deltaTime, 0, 0);
+                break;
+            case States.Turn:
+                Vector3 pos = Vector3.zero;
+                pos.x = bezierTurn.CalculateCubicBezierPoint(speed).x;
+                pos.y = transform.position.y;
+                pos.x = bezierTurn.CalculateCubicBezierPoint(speed).z;
+                transform.position = pos;
+                transform.LookAt(new Vector3(bezierTurn.LookAtPoint().x, transform.position.y, bezierTurn.LookAtPoint().z));
+                if (states == States.Forward)
+                {
+                    transform.eulerAngles = bezierTurn.GetFixedRotation();
+                    bezierTurn = null;
+                }                
+                break;
+            default:
+                break;
+        }
+    }
+} 
