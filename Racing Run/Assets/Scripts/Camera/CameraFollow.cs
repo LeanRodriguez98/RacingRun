db852a1715368922446722549cd536471e85ed55
Unknown <lean1998@hotmail.com>
2018-11-10 17:21:30 -0300

    CameraSmoothness

--- Racing Run/Assets/Scripts/Camera/CameraFollow.cs
+++ Racing Run/Assets/Scripts/Camera/CameraFollow.cs
@@ -11,11 +11,27 @@ public class CameraFollow : MonoBehaviour
     public bool followBehind;
     public float rotationDamping;
     public Car carInstance;
+    private Gyroscope gyroscope;
+    private bool gyroscopeEnabled;
+
     private void Start()
     {
         carInstance = Car.instance;
         target = carInstance.transform;
+        gyroscopeEnabled = EnableGyro();
     }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+            return true;
+        }
+        return false;
+    }
+
     void LateUpdate()
     {
         if (carInstance != null)
@@ -26,8 +42,12 @@ public class CameraFollow : MonoBehaviour
             else
                 wantedPosition = target.TransformPoint(0, height, distance);
 
-            transform.position = wantedPosition;// Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
-
+            transform.position = /*wantedPosition;*/ Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+            if (gyroscopeEnabled)
+            {
+                Debug.Log("Gyro");
+                transform.eulerAngles = new Vector3(0, 0, gyroscope.attitude.eulerAngles.x);
+            }
             if (smoothRotation)
             {
                 Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
